bacb56740e709211db2a69d7cb2b648d5eef88f1
Kirill Sysoev <kirill.sysoev@gmail.com>
2013-07-05 00:05:32 +0400

    U

--- z_mydb.erl
+++ z_mydb.erl
@@ -14,7 +14,7 @@
 q(Query,Context) ->
     q(Query,[],Context).
 q(Query,Parameters,Context) ->
-    EMySQLPool = binary_to_list(m_config:get_value(mod_mysql, pool_name, Context)),
+    EMySQLPool = list_to_atom(binary_to_list(m_config:get_value(mod_mysql, pool_name, Context))),
     emysql:prepare(zmydb_query,Query),
     {_,_,_,Result,_} = emysql:execute(EMySQLPool, zmydb_query, Parameters),
     Result.
@@ -22,7 +22,7 @@ q(Query,Parameters,Context) ->
 q_raw(Query,Context) ->
     q_raw(Query,[],Context).
 q_raw(Query,Parameters,Context) ->
-    EMySQLPool = binary_to_list(m_config:get_value(mod_mysql, pool_name, Context)),
+    EMySQLPool = list_to_atom(binary_to_list(m_config:get_value(mod_mysql, pool_name, Context))),
     emysql:prepare(zmydb_query,Query),
     emysql:execute(EMySQLPool, zmydb_query, Parameters).
 
